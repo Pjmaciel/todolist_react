ffc71bf16015f8fc2ea98ff9ad66b5f49fd2f434
Pjmaciel <pjmaciel_4@hotmail.com>
2023-09-14 19:49:50 -0300

    Create function Delete by click on icon trash

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,15 @@ import { ITask } from "./interfaces/Task";
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([]);//Começa com um array vazio
 
+  const deleteTask = (id: number) => {
+      setTaskList(
+        taskList.filter(task =>{
+          return task.id !== id
+        })
+      );
+  };
+
+
   return <div>
     <Header />
     <main className={styles.main}>
@@ -26,7 +35,7 @@ function App() {
       </div>
       <div>
         <h2>Suas Tarefas:</h2>
-        <TaskList taskList={taskList}/>
+        <TaskList taskList={taskList} handleDelete={deleteTask}/>
       </div>
     </main>
     <Footer />
