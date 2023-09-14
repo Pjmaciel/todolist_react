5ec23285d5073257c4a5a0fcc35f0afd9bcd51e7
Pjmaciel <pjmaciel_4@hotmail.com>
2023-09-14 18:59:49 -0300

    Config view of tasks and add icon bootstrap

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,7 +14,15 @@ const TaskList = ({taskList}: Props) => {
         {taskList.length > 0 ? (
             taskList.map((task) => (
                 <div key={task.id}>
-                    <p>{task.title}</p>
+                    <div>
+                    <h4>{task.title}</h4>
+                    <p>Dificuldade: {task.difficulty}</p>
+                    </div>
+                    <i className="bi bi-pencil"></i>
+                    <i className="bi bi-trash"></i>
+                    <div>
+
+                    </div>
                 </div>
             ))
         ) : (
