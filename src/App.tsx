347cd6800aaf0d8ad822d7e74ec2e34e571706af
Pjmaciel <pjmaciel_4@hotmail.com>
2023-09-14 21:12:03 -0300

    add modal and configurated button edit from hide or show modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 //Components
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-
-//Components forms
+import Modal from "./components/Modal";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
+
 
 //css
 import styles from "./App.module.css";
@@ -17,16 +17,29 @@ import { ITask } from "./interfaces/Task";
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([]);//Começa com um array vazio
 
-  const deleteTask = (id: number) => {
+  const iconDeleteTask = (id: number) => {
       setTaskList(
         taskList.filter(task =>{
           return task.id !== id
         })
       );
   };
+  
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if(display){
+        modal!.classList.remove("hide");
+    }else{
+        modal!.classList.add("hide");
+    }
+}
 
+  const iconEditTask = ():void => {
+    hideOrShowModal(true);
+}
 
   return <div>
+    <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList}/>}/>
     <Header />
     <main className={styles.main}>
       <div>
@@ -35,7 +48,7 @@ function App() {
       </div>
       <div>
         <h2>Suas Tarefas:</h2>
-        <TaskList taskList={taskList} handleDelete={deleteTask}/>
+        <TaskList taskList={taskList} handleDelete={iconDeleteTask} handleEdit={iconEditTask}/>
       </div>
     </main>
     <Footer />
